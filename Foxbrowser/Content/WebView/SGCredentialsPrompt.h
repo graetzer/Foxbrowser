5964bddc3ba848a86a6f0871cd177cf9e58725a5
Simon Grätzer <simon@graetzer.org>
2012-08-20 21:41:58 +0200

    Added custom NSURLProtocol

---
+++ Foxbrowser/Content/WebView/SGCredentialsPrompt.h
@@ -0,0 +1,13 @@
+//
+//  SGCredentialsPrompt.h
+//  Foxbrowser
+//
+//  Created by Simon Grätzer on 20.08.12.
+//  Copyright (c) 2012 Simon Peter Grätzer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SGCredentialsPrompt : UIAlertView
+
+@end
---
+++ Foxbrowser/Content/WebView/SGCredentialsPrompt.m
@@ -0,0 +1,31 @@
+//
+//  SGCredentialsPrompt.m
+//  Foxbrowser
+//
+//  Created by Simon Grätzer on 20.08.12.
+//  Copyright (c) 2012 Simon Peter Grätzer. All rights reserved.
+//
+
+#import "SGCredentialsPrompt.h"
+
+@implementation SGCredentialsPrompt
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
